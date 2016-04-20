2a3bddba1d5291f29e7044b31e7abfc398b9b152
Jesus Gollonet <jesusgollonet@gmail.com>
2016-04-20 13:32:23 +0200

    Fix example to write photo + overlay

--- node-examples/clm_image.js
+++ node-examples/clm_image.js
@@ -1,13 +1,13 @@
 var clm = require('../js/clm.js');
 var Canvas = require('canvas');
 var fs = require('fs');
-var pModel = JSON.parse(fs.readFileSync(__dirname + '/../models/model_pca_20_svm.json', 'utf8'));
+var pModel = require('../models/model_pca_20_svm.json');
 
 var Image = Canvas.Image;
 var canvas = new Canvas(625,500);
 var overlay = new Canvas(625,500);
 var ctx = canvas.getContext('2d');
-var overlayCtx = canvas.getContext('2d');
+var overlayCtx = overlay.getContext('2d');
 
 var img = new Image();
 var imgFile = fs.readFileSync(__dirname + '/../examples/media/franck_01829.jpg');
@@ -27,6 +27,7 @@ ctrack.emitter.on('clmtrackrIteration', function(){
 
 ctrack.emitter.on('clmtrackrConverged', function(){
     overlayCtx.clearRect(0, 0,625, 500);
+    overlayCtx.drawImage(img, 0, 0);
     ctrack.draw(overlay);
     var out = fs.createWriteStream(__dirname + '/test.png');
     var stream = overlay.createPNGStream();
